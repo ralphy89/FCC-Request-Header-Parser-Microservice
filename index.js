44a1486c62dba81a75bff545d5d746cb4bd1479f
Ralphy <ralphdumera00@gmail.com>
2024-10-03 02:32:00 +0000

    Update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
+const UserAgent = require("user-agents");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -22,6 +23,16 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami/', function (req, res) {
+  let ipadress = req.ip.toString();
+  let language = req.acceptsLanguages().toString();
+  let sofware = new UserAgent().toString();
+  let data = {};
+  console.log("IP : " + req.ip + "\nlanguage : " + req.acceptsLanguages() + "\nsoftware : " + sofware.toString());
+  data = {"ipaddress": ipadress, "language":language, "software": sofware};
+  res.json(data);
 });
 
 // listen for requests :)
